fefb57cde3187f746446ffd5b5418cf1df03feab
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-01 08:00:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes[:title].blank?}
   validates_presence_of :name, :content
 
-  def tags_attributes=(tags_attributes)
-    tags_attributes.each do |attributes|
+  def tags_attributes=(tags)
+    tags.each do |attributes|
       self.tags << Tag.find_or_create_by(attributes)
     end
   end
